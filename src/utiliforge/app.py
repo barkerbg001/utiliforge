710a123082a67834ff567b840a7de84a1c59d05d
Bradley <barkerbg001@gmail.com>
2025-01-20 21:50:26 +0200

    Add initial project structure and release notes for UtiliForge

---
+++ src/utiliforge/app.py
@@ -0,0 +1,26 @@
+"""
+A cross-platform utility suite combining productivity tools and developer features, designed to streamline workflows and enhance system functionality.
+"""
+
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+
+class UtiliForge(toga.App):
+    def startup(self):
+        """Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return UtiliForge()
